2b2ada0eaf6d16f5a6224c48f65f51b18a2a0d53
Cory <coryf65@gmail.com>
2021-02-04 18:59:55 -0600

    Basics of Properties

--- Assets/Scripts/1 Creating Properties/Player.cs
+++ Assets/Scripts/1 Creating Properties/Player.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Basics of a Property
+
+    private int experience;
+
+    public int Experience
     {
-        
+        // Accessor
+        get
+        {
+            // Some other code
+            return experience;
+        }
+        set
+        {
+            // Some other Code
+            experience = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Level
     {
-        
+        get
+        {
+            // return numerical level not total EXP
+            return experience / 1000;
+        }
+        set
+        {
+            experience = value * 1000;
+        }
     }
+
+    // Shorthand Property, that can act like a field
+    public int Health { get; set; }
+
+
 }
